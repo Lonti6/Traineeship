512562a3f21554aea6fb9cce42b712068c16a1a4
Lonti6 <72629247+Lonti6@users.noreply.github.com>
2022-04-10 20:21:50 +0500

    доделал настройки

--- app/src/main/java/ru/work/trainsheep/SettingsActivity.java
+++ app/src/main/java/ru/work/trainsheep/SettingsActivity.java
@@ -1,12 +1,22 @@
 package ru.work.trainsheep;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.mxn.soul.flowingdrawer_core.ElasticDrawer;
 import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
@@ -48,6 +58,15 @@ public class SettingsActivity extends AppCompatActivity {
             if (direction.name().equals("LEFT"))
                 stickySwitchVacansy.setSliderBackgroundColor(getColor(R.color.line_grey));
         });
+
+        findViewById(R.id.delete_text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getSupportFragmentManager();
+                MyDialogFragment myDialogFragment = new MyDialogFragment();
+                myDialogFragment.show(manager, "Frag");
+            }
+        });
     }
 
     private void initToggleBut(StickySwitch stickySwitch)
@@ -56,5 +75,22 @@ public class SettingsActivity extends AppCompatActivity {
             stickySwitch.setSliderBackgroundColor(getColor(R.color.light_green));
         if (stickySwitch.getDirection().name().equals("LEFT"))
             stickySwitch.setSliderBackgroundColor(getColor(R.color.line_grey));
+    }
+
+    public static class MyDialogFragment extends DialogFragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            getDialog().getWindow().setBackgroundDrawableResource(R.drawable.round_corner);
+            return inflater.inflate(R.layout.dialog_layout, container, false);
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            int width = (int)(getResources().getDisplayMetrics().widthPixels*0.85);
+            int heights = (int)(getResources().getDisplayMetrics().heightPixels*0.4);
+            getDialog().getWindow().setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
     }
 }
