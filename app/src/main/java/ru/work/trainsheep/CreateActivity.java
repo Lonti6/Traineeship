b009ef65f5775bde087c461082aa9a3a62c46e63
Lonti6 <72629247+Lonti6@users.noreply.github.com>
2022-04-15 21:41:01 +0500

    теги добавляются в создании вакансии

--- app/src/main/java/ru/work/trainsheep/CreateActivity.java
+++ app/src/main/java/ru/work/trainsheep/CreateActivity.java
@@ -1,10 +1,19 @@
 package ru.work.trainsheep;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
+
+import org.apmem.tools.layouts.FlowLayout;
+
+import lombok.val;
 
 public class CreateActivity extends AppCompatActivity {
     @Override
@@ -20,5 +29,27 @@ public class CreateActivity extends AppCompatActivity {
         Util.setEditTextFocusListener(this, R.id.zpField);
         Util.setEditTextFocusListener(this, R.id.workTimeField);
         Util.setEditTextFocusListener(this, R.id.descriptionField);
+
+        AutoCompleteTextView mAutoCompleteTextView;
+        final String[] mCats = { "Мурзик", "Рыжик", "Барсик", "Борис",
+                "Мурзилка", "Мурка", "Муму", "Матроскин" };
+
+
+        mAutoCompleteTextView = findViewById(R.id.competenciesField);
+        mAutoCompleteTextView.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, mCats));
+
+        FlowLayout tagsField = (FlowLayout)findViewById(R.id.tags_field);
+        tagsField.setVisibility(View.GONE);
+
+        TextView textView = (TextView)findViewById(R.id.competenciesField);
+
+        findViewById(R.id.addBut).setOnClickListener(v -> {
+            val view = LayoutInflater.from(v.getContext()).inflate(R.layout.tag_item, tagsField, false);
+            tagsField.addView(view);
+            ((TextView) view.findViewById(R.id.tag)).setText(textView.getText());
+            tagsField.setVisibility(View.VISIBLE);
+            textView.setText("");
+        });
     }
 }
