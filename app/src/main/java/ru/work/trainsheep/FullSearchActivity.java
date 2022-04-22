e2d79d6d1aa82d426b1b4fa256c3e45a0e203218
Lonti6 <72629247+Lonti6@users.noreply.github.com>
2022-04-23 00:13:22 +0500

    + страница полного поиска

--- app/src/main/java/ru/work/trainsheep/FullSearchActivity.java
+++ app/src/main/java/ru/work/trainsheep/FullSearchActivity.java
@@ -1,6 +1,21 @@
 package ru.work.trainsheep;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
+
 public class FullSearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_left_panel);
+        final FlowingDrawer drawer = Util.connectActivityLayout(this, R.layout.activity_full_search);
+
+        findViewById(R.id.scroller).setOnScrollChangeListener(new ScrollListeners.MyScrollListener(findViewById(R.id.header),
+                getDrawable(R.drawable.bg_header)));
+
+        findViewById(R.id.menuBut).setOnClickListener(v -> Util.loadActivity(drawer, this, SearchActivity.class));
+    }
 }
